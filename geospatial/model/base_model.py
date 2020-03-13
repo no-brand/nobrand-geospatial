dde9811aa14836e3f74bfad2824f7edaabb2bcf2
no-brand <do.dream.david@gmail.com>
2020-03-14 00:06:29 +0900

    feat(base-model): add abstractmethod to be scalable

--- geospatial/model/base_model.py
+++ geospatial/model/base_model.py
@@ -2,17 +2,32 @@
 
 from boto3.dynamodb.types import *
 from abc import ABC, abstractmethod
+import json
 
 
 class BaseModel(ABC):
+
+    def __str__(self):
+        params = self.__dict__()
+        return json.dumps(params)
 
     @abstractmethod
     def __dict__(self):
         pass
 
+    @abstractmethod
+    def prepare_serialize_to_ddb(self, params=None):
+        pass
+
+    @abstractmethod
+    def prepare_serialize_to_elastic(self, params=None):
+        pass
+
     def serialize_to_ddb(self, type_nested=False):
         """
         get dict, representing a dynamodb data type.
+        on type_nested is True condition, dict is quite different from serialize_to_elastic.
+        this function cast all the float values to Decimal.
         :param type_nested: boolean, to determine between original dict and dynamodb's type defined type.
 
             type_nested = False (Python)            type_nested = True (DynamoDB)
@@ -41,6 +56,7 @@ class BaseModel(ABC):
             return TypeSerializer().serialize(x) if type_nested else x
 
         params = self.__dict__()
+        params = self.prepare_serialize_to_ddb(params=params)
         return {k: __serialize(v) for k, v in params.items()}
 
     def serialize_to_elastic(self):
@@ -50,4 +66,5 @@ class BaseModel(ABC):
         :returns: a dictionary that can be directly passed to index into elasticsearch.
         """
         params = self.__dict__()
+        params = self.prepare_serialize_to_elastic(params=params)
         return params
